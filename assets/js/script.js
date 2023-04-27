c56eb99fde3cc1980d80372387d5f10cb52346c9
Kamal-Kohli <>
2023-04-27 10:51:24 +0000

    Add style and hover to Option Images

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,4 @@ var yourScore = 0;
 var opponent;
 var opponentScore = 0;
 
-var choices = ["R", "S", "P"];
-
-window.onload = function() {
-    for (let i = 0; i < 3; i++) {
-        // <img id="r" src="assets/images/r.png">
-        let choice = document.createElement("img");
-        choice.id = choices[i];
-        choice.src = choices[i] + ".png";
-        choice.addEventListener("click", selectChoice);
-        document.getElementByClass("choices").append(choice);
-    }
-}
+var optionChoices = ["R", "S", "P"];
