bbb9c00d22680c775ce5e77dd6ceae049a919160
Kamal-Kohli <>
2023-05-01 14:54:24 +0000

    Add question to Script

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// List of Questions
+const quiz = [
+    {
+        q:'Inside which HTML element do we put the JavaScript?',
+        options:['if i == 5 then','if i = 5 then','if (i == 5)','if i = 5'],
+        answer: 2
+    },
+    {
+        q:'What is the correct JavaScript syntax to change the content of the HTML element below?',
+        options:['document.getElementById("demo").innerHTML = "Hello World!"','document.getElement("p").innerHTML = "Hello World!";','#demo.innerHTML = "Hello World!";','document.getElementByName("p").innerHTML = "Hello World!";'],
+        answer: 0
+    },
+    {
+        q:'Where is the correct place to insert a JavaScript?',
+        options:['The <body> section','Both the <head> section and the <body> section are correct','The <head> section','None of Above'],
+        answer: 1
+    },
+    {
+        q:'What is the correct syntax for referring to an external script called "xxx.js"?',
+        options:['<script name="xxx.js">','<script href="xxx.js">','<script src="xxx.js">','None of Above'],
+        answer: 2
+    },
+    {
+        q:'How do you write "Hello World" in an alert box?',
+        options:['msg("Hello World");','alertBox("Hello World");','alert("Hello World");','msgBox("Hello World");'],
+        answer: 2
+    },
+]
