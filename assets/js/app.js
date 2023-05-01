b2f88458c3b47d301a7f61973bb9f6549f032c29
Kamal-Kohli <>
2023-05-01 16:40:26 +0000

    Push option into availableOPtions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
-const questionContainer = document.querySelector(".option-container");
+const optionContainer = document.querySelector(".option-container");
 
 let questionCounter = 0;
-let currectQuestion;
+let currentQuestion;
 let availableQuestions = [];
+let availableOptions = [];
 
 // availableQuestion Array
 function setAvailabeQuestions(){
@@ -16,14 +17,33 @@ function setAvailabeQuestions(){
 
 // set question, options, and question number
 function getNewQuestion(){
-    questionNumber.innerHTML = "Question" + (questionCounter + 1) + "of" + quiz.length;
+    // Question Number
+    questionNumber.innerHTML = " Question " + (questionCounter + 1) + " of " + quiz.length;
 
 
     // Question and Set random question
     const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
-    currectQuestion = questionIndex;
-    questionText.innerHTML = currectQuestion.q;
-    console.log(questionIndex);
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+
+    //get the position of Question from availableQuestion Array
+    const index1= availableQuestions.indexOf(questionIndex);
+
+    // remove the questionIndex from availableQuestion for doesn't repeat the question
+    availableQuestions.splice(index1,1);
+    // push options to availableOptions Array
+    const optionLen = currentQuestion.options.length
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+    }
+
+    for(let i=0; i<optionLen; i++){
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[i];
+        option.id = i;
+        option.className = "option";
+        optionContainer.appendChild(option)
+    }
 
     questionCounter++
 }
@@ -31,7 +51,8 @@ function getNewQuestion(){
 function next(){
     if(questionCounter === quiz.length){
         console.log("quiz over")
-    } else{
+    } 
+    else{
         getNewQuestion();
     }
 }
@@ -39,6 +60,6 @@ function next(){
 window.onload = function(){
 
     setAvailabeQuestions();
-    
+
     getNewQuestion();
 }
