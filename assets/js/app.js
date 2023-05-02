6c7fd2ec233e862026a69e1704d546148decb99d
Kamal-Kohli <>
2023-05-02 11:37:38 +0000

    Add Try again, Home, and start button in JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,19 @@
+
 const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
 const optionContainer = document.querySelector(".option-container");
+const answersIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+
 
 let questionCounter = 0;
 let currentQuestion;
 let availableQuestions = [];
 let availableOptions = [];
+let correctAnswers = 0;
+let attempt = 0;
 
 // availableQuestion Array
 function setAvailabeQuestions(){
@@ -36,17 +44,18 @@ function getNewQuestion(){
     for(let i=0; i<optionLen; i++){
         availableOptions.push(i)
     }
-
-    let animationDelay = 0.2;
+    optionContainer.innerHTML = '';
+    let animationDelay = 0.15;
     // Create optiond in HTML
     for(let i=0; i<optionLen; i++){
         //random otions
         const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
         
+        // get the position of optionIndex from the availableOptions
         const index2 = availableOptions.indexOf(optionIndex);
-        
+
+        // Remove the optionIndex from the availableOptions.
         availableOptions.splice(index2,1);
-    
         const option = document.createElement("div");
         option.innerHTML = currentQuestion.options[optionIndex ];
         option.id = optionIndex;
@@ -65,6 +74,8 @@ function getResult(element){
     const id = parseInt(element.id);
     if(id === currentQuestion.answer){
         element.classList.add("correct");
+        updateAnswerIndicator("correct");
+        correctAnswers++;
     }
     else{
         element.classList.add("wrong");
@@ -109,13 +120,40 @@ function quizResult(){
     resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
     resultBox.querySelector(".total-wrong").innerHTML = attempt - correctAnswers;
     const percentage = (correctAnswers/quiz.length)*100;
-    resultBox.querySelector(".percentage").innerHTML =percentage.toFixed() + "%";
+    resultBox.querySelector(".percentage").innerHTML =percentage.toFixed(2) + "%";
     resultBox.querySelector(".total-score").innerHTML =correctAnswers +" / " + quiz.length;
 } 
 
-window.onload = function(){
+function reset(){
+    questionCounter = 0;
+    correctAnswers = 0;
+    attempt = 0;
+}
+
+function tryAgainQuiz(){
+    resultBox.classList.add("hide");
+    quizBox.classList.remove("hide");
+    resetQuiz();
+    startQuiz();
+}
+
+function goToHome(){
+    resultBox.classList.add("hide");
+    quizBox.classList.remove("hide");
+    resetQuiz();
+}
+
+function  startQuiz(){
+    homeBox.classList.add("hide");
+    quizBox.classList.remove("hide");
 
     setAvailabeQuestions();
-
     getNewQuestion();
+    answersIndicator();
+
+}
+
+
+window.onload = function(){
+    homeBox.querySelector(".total-question").innerHTML = quiz.length;
 }
